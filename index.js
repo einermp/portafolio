5e2cceb16990f5d302803cfa8c27b0c4fe3271ea
einerm <einermendez941128@gmail.com>
2025-05-04 10:27:04 -0500

    Ajustes de estilos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Ok");
 var mainVue = new Vue({
-    el: "#minDiv",
+    el: "#mainDiv",
     data: {
 
     },
@@ -8,17 +8,46 @@ var mainVue = new Vue({
         console.log("Holaa...")
     },
     methods: function () {
-
     }
 });
 
 const toggleButton = document.getElementById('toggle-button');
 toggleButton.addEventListener('change', () => {
     document.body.classList.toggle('dark');
-    localStorage.setItem("dark-mode",  document.body.classList);
+    localStorage.setItem("dark-mode", document.body.classList);
 });
 
 if (localStorage.getItem('dark-mode') === 'dark') {
     document.body.classList.toggle('dark');
     toggleButton.checked = true;
 }
+
+let items = document.querySelectorAll(".items");
+const secciones = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const id = entry.target.id;
+            items.forEach((item) => {
+                item.classList.remove("items-active");
+                if(item.parentNode.href.includes(id)){
+                    item.classList.add("items-active");
+                }
+            });
+        }
+    });
+}, {
+    threshold: 0.5, // Ajusta el umbral de visibilidad
+});
+
+secciones.forEach((seccion) => observer.observe(seccion));
+
+items.forEach((item) => {
+    item.addEventListener("click", () => {
+        items.forEach((e) => {
+            e.classList.remove("items-active");
+            item.classList.add("items-active");
+        });
+    });
+});
